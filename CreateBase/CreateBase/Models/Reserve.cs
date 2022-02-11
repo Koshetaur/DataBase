df702c368aa4a8d00ba270797df35ee6780e19b1
Koshetaur <60685097+Koshetaur@users.noreply.github.com>
2022-02-11 17:46:29 +0700

    Add files via upload

---
+++ CreateBase/CreateBase/Models/Reserve.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CreateBase
+{
+    public class Reserve
+    {
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        [Required]
+        public int RoomId { get; set; }
+        [ForeignKey("RoomId")]
+        public Room Room { get; set; }
+        public DateTime TimeStart { get; set; }
+        public DateTime TimeEnd { get; set; }
+    }
+}

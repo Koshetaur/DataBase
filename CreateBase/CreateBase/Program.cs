7a4a2cd7a31b3f21f3e2faa701b2afe1fd91c769
Koshetaur <60685097+Koshetaur@users.noreply.github.com>
2022-02-11 17:49:02 +0700

    Add files via upload

---
+++ CreateBase/CreateBase/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CreateBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (IRepository db = new Repository())
+            {
+                /*db.CreateUser("Sergey", "Razumovskiy");
+                db.CreateUser("Oleg", "Volkov");
+                db.CreateRoom("Room A");
+                db.CreateRoom("Room B");
+                db.Save();
+                var start = new DateTime(2022, 02, 12, 13, 15, 00);
+                var end = new DateTime(2022, 02, 12, 13, 40, 00);
+                db.CreateReserve(1, 2, start, end);
+                db.Save();*/
+                var connections = db.GetReserveList();
+                foreach (Reserve c in connections)
+                {
+                    Console.Write($"{c.Id}. {c.User.Name} {c.User.Surname} reserved {c.Room.Name} from {c.TimeStart} to {c.TimeEnd}.\n");
+                }
+                Console.Read();
+            }
+        }
+    }
+}

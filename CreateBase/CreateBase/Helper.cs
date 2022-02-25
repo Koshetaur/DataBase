a3e4a2aabdb6c8522e4c11aa3b5ed825f82594ed
shugeka21@outlook.com <shugeka21@outlook.com>
2022-02-25 10:50:52 +0700

    Изменена строка подключения

--- CreateBase/CreateBase/Helper.cs
+++ CreateBase/CreateBase/Helper.cs
@@ -6,6 +6,6 @@ namespace CreateBase
 {
     public class Helper
     {
-        public static string ConnectionString = "Data Source=C:/Users/shoog/OneDrive/Рабочий стол/CreateBase/CreateBase/usersandrooms.db";
+        public static string ConnectionString = "Data Source=..\\..\\..\\usersandrooms.db";
     }
 }
